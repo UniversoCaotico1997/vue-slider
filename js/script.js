8eab6a9220106433d8132377867e00056aa4df04
UniversoCaotico1997 <loria.roberto97@gmail.com>
2022-10-27 16:16:30 +0200

    aggiunsione di v-for nel markup

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const slides = [
-{
-    image: 'img/01.webp',
+    {
+        image: 'img/01.webp',
         title: 'Marvel\'s Spiderman Miles Morale',
         text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
     }, {
@@ -22,7 +22,6 @@ const slides = [
     }
 ];
 
-console.log(slides);
 
 
 // Descrizione:
@@ -30,7 +29,10 @@ console.log(slides);
 
 // Iniziamo collegando la CDN al nonstro markup 
 // Creiamo una lista di oggetti al suo interno 
-createApp({
+
+const { createApp } = Vue
+
+  createApp({
     data() {
       return {
         movies:[
@@ -40,22 +42,22 @@ createApp({
                 text:'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
             },
             {
-                image: 'img/02.webp',
+                image:'img/02.webp',
                 title: 'Ratchet & Clank: Rift Apart',
                 text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-            }, 
+            },
             {
-                image: 'img/03.webp',
+                image:'img/03.webp',
                 title: 'Fortnite',
                 text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-            }, 
+            },
             {
-                image: 'img/04.webp',
+                image:'img/04.webp',
                 title: 'Stray',
                 text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-            }, 
+            },
             {
-                image: 'img/05.webp',
+                image:'img/05.webp',
                 title: "Marvel's Avengers",
                 text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
             }
@@ -63,6 +65,9 @@ createApp({
       }
     }
   }).mount('#app')
+
+
+
 
 // Bonus:
 // al click su una thumb, visualizzare in grande l'immagine corrispondente
