05331e0717a0f861e093f15551fcaa8939210374
UniversoCaotico1997 <loria.roberto97@gmail.com>
2022-10-28 10:40:04 +0200

    aggiunzione dello sliders continuo

--- js/script.js
+++ js/script.js
@@ -1,34 +1,7 @@
-const slides = [
-    {
-        image: 'img/01.webp',
-        title: 'Marvel\'s Spiderman Miles Morale',
-        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-    }, {
-        image: 'img/02.webp',
-        title: 'Ratchet & Clank: Rift Apart',
-        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-    }, {
-        image: 'img/03.webp',
-        title: 'Fortnite',
-        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-    }, {
-        image: 'img/04.webp',
-        title: 'Stray',
-        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-    }, {
-        image: 'img/05.webp',
-        title: "Marvel's Avengers",
-        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-    }
-];
-
-
-
 // Descrizione:
 // Partendo dal markup della versione svolta in js plain (vedi allegato), rifare lo slider ma questa volta usando Vue.
 
-// Iniziamo collegando la CDN al nonstro markup 
-// Creiamo una lista di oggetti al suo interno 
+
 
 const { createApp } = Vue
 
@@ -64,12 +37,19 @@ createApp({
     methods:{
         prevImage(){
             // console.log(`immagine precedente`);
-            this.index--
+            this.index--  
+            if (this.index < 0) {
+                this.index = this.movies.image.length -1
+                
+            } 
         },
         nextImage(){
             // console.log(`immagine successiva`);
             this.index++
+            if (this.index === this.movies.length) {
+                this.index = 0 
+                
+            }
         }
-
     }
 }).mount('#app')
